0d27711b771f604f35eb991a52d415b1c93fefa1
zezking <zhaoenze001@gmail.com>
2021-02-27 07:55:08 -0800

    move navigation to index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'font-awesome/css/font-awesome.min.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 import './index.css';
 
-const fontAwsomeComponent=function() {
-    return <div><i className="fa fa-spinner fa-spin">no spinner but why</i></div>;
+class Navigation extends React.Component {
+    render(){
+       return ( <div class="navigation">
+        <nav>
+          <a href="">pomodoro</a>
+          <a href="">short break</a>
+          <a href="">long break</a>
+        </nav>
+      </div>);
+    }
 }
