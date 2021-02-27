e3f0e311c9a186fc4ae87d214028b4caebe7062d
zezking <zhaoenze001@gmail.com>
2021-02-26 22:49:37 -0800

    import font awesome

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,3 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css';
+
 import './index.css';
+
+const fontAwsomeComponent=function() {
+    return <div><i className="fa fa-spinner fa-spin">no spinner but why</i></div>;
+}
