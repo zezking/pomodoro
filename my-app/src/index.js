90a1bef4291a63ff3d24bb1659c7adc0938fec2f
zezking <zhaoenze001@gmail.com>
2021-02-28 18:24:52 -0800

    add a button component in index.js

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-import './index.css';
+import "./index.css";
 
-class Navigation extends React.Component {
-    render(){
-       return ( <div class="navigation">
-        <nav>
-          <a href="">pomodoro</a>
-          <a href="">short break</a>
-          <a href="">long break</a>
-        </nav>
-      </div>);
-    }
-}
+const Button = () => {
+  return (
+    <div class="navigation">
+      <nav>
+        <a href="">pomodoro</a>
+        <a href="">short break</a>
+        <a href="">long break</a>
+      </nav>
+    </div>
+  );
+};
+
+ReactDOM.render(<Button />, document.querySelector("#root"));
