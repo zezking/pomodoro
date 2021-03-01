4d913e39973870d33cce0db953bbbac8df705f2f
zezking <zhaoenze001@gmail.com>
2021-02-28 18:48:30 -0800

    remove div outside of div #root

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
-import "./index.css";
+import "./style.css";
 
 const Button = () => {
   return (
